30fbceaf5a323245e22bb60012997bd77820ea5d
Dmitriy Olin <DOlin@synergy.ru>
2020-10-13 13:25:09 +0300

    Updates

---
+++ precache-manifest.f2e7c85b53a28dcddadede65aae6ac5b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6e396bc30e1840ebd8a9cc6f04d708",
+    "url": "/funbox/index.html"
+  },
+  {
+    "revision": "5d6e68be3d1f5be2a2d4",
+    "url": "/funbox/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "23b5f120e2d42d03fa97",
+    "url": "/funbox/static/css/main.961f61a0.chunk.css"
+  },
+  {
+    "revision": "5d6e68be3d1f5be2a2d4",
+    "url": "/funbox/static/js/2.d3abf554.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/funbox/static/js/2.d3abf554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b5f120e2d42d03fa97",
+    "url": "/funbox/static/js/main.0e23470f.chunk.js"
+  },
+  {
+    "revision": "9cb1d724b9db784c1406",
+    "url": "/funbox/static/js/runtime-main.c6ed7ab8.js"
+  },
+  {
+    "revision": "fe0bd159f580886def64665c9b2e8be1",
+    "url": "/funbox/static/media/bg.fe0bd159.jpg"
+  },
+  {
+    "revision": "0a5db29f7face36544ac8f6432a3e8b4",
+    "url": "/funbox/static/media/cat.0a5db29f.png"
+  },
+  {
+    "revision": "54523f78c22126510bd2e6a6535cb14a",
+    "url": "/funbox/static/media/exo2.0-thin-webfont.54523f78.woff"
+  },
+  {
+    "revision": "a283cd08d4c5f281e4c761280d0a5939",
+    "url": "/funbox/static/media/exo2.0-thin-webfont.a283cd08.woff2"
+  },
+  {
+    "revision": "4e3e6fb907b3e8f384a1abf29f87639b",
+    "url": "/funbox/static/media/trebuchet-bolditalic-webfont.4e3e6fb9.woff"
+  },
+  {
+    "revision": "f196fcb7d83dd2836d00dae07c86e8b8",
+    "url": "/funbox/static/media/trebuchet-bolditalic-webfont.f196fcb7.woff2"
+  },
+  {
+    "revision": "5d1a99107835c4c5dd9e86a9ff63ab6b",
+    "url": "/funbox/static/media/trebuchetms-bold-webfont.5d1a9910.woff"
+  },
+  {
+    "revision": "8fa5f09f9daf07da548125910236695c",
+    "url": "/funbox/static/media/trebuchetms-bold-webfont.8fa5f09f.woff2"
+  },
+  {
+    "revision": "b9b11007e7444efd8111cce8d723d98b",
+    "url": "/funbox/static/media/trebuchetms-italic-webfont.b9b11007.woff"
+  },
+  {
+    "revision": "c4e3b8f5c998c3106eb4c38e9900fad4",
+    "url": "/funbox/static/media/trebuchetms-italic-webfont.c4e3b8f5.woff2"
+  },
+  {
+    "revision": "1ff40c3cd38bf87cc9991beaf313bd4e",
+    "url": "/funbox/static/media/trebuchetms-webfont.1ff40c3c.woff"
+  },
+  {
+    "revision": "a101faeace9e75321fd06ac3cbfddaf5",
+    "url": "/funbox/static/media/trebuchetms-webfont.a101faea.woff2"
+  }
+]);
